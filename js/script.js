14df3727cb0b0352fda65a5c1b4e5d869c10b1f6
Francesco-Matteucci <francescomatteucci@gmail.com>
2024-09-11 15:52:06 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -10,14 +10,22 @@ createApp({
     methods: {
         // Metodo per chiamare l'API e aggiungere l'email all'array
         generateEmails() {
+            // Numero delle email da generare
+            const emailCount = 10;
             // Eseguo un ciclo per ottenere 10 email random
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < emailCount; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                     .then(response => {
                         // Aggiungo l'email all'array
                         this.emails.push(response.data.response);
                     })
             }
+        }
+    },
+    computed: {
+        // Eseguo una verifica per vedere se tutte le mail sono state generate
+        allEmailsGenerated() {
+            return this.emails.length === 10;
         }
     },
     mounted() {
